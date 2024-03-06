e88add9258f25e75541ff1370deaa89599eefdd4
thaliawoods <thaliadelano@gmail.com>
2024-03-06 14:38:10 +0100

    ListStart

---
+++ todolistreacttypescript/src/TodoList.tsx
@@ -0,0 +1,14 @@
+
+  export const TodoList = () => {
+    return (
+    <div className="main-container">
+      <h1>Todo List</h1>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+      </ul>
+      <input type="text" placeholder="Add todo item"/>
+      <button>Add</button>
+    </div>
+    )
+  }
